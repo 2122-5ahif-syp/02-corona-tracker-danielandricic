6934fb6cac608838b0384cd822b5c7b0017d32e7
danielandricic <dani.andricic@gmail.com>
2021-10-21 14:18:12 +0200

    add entity

--- src/main/java/at/htl/entity/Visitor.java
+++ src/main/java/at/htl/entity/Visitor.java
@@ -1,5 +1,83 @@
 package at.htl.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity(name = "visitor")
 public class Visitor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phoneNumber;
+    private Instant timestamp;
+
+    public Visitor(String firstname, String lastname, String email, String phoneNumber) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Visitor() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public String getFullName() {
+        return String.format("%s %s", getFirstname(), getLastname());
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Visitor with name %s, with ID: %d", getFullName(), getId());
+    }
 }
